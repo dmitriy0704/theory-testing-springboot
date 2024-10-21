f5e9f4e21c3525801f9b1e28b26ad4a665ef4473
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-10-21 23:02:25 +0300

    init new module sandbox

---
+++ sandbox-spring-openapi/src/main/java/dev/folomkin/sandbox/SandboxSpringOpenapiApplication.java
@@ -0,0 +1,13 @@
+package dev.folomkin.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxSpringOpenapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxSpringOpenapiApplication.class, args);
+    }
+
+}
---
+++ sandbox-spring-openapi/src/test/java/dev/folomkin/sandbox/SandboxSpringOpenapiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.folomkin.sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxSpringOpenapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
