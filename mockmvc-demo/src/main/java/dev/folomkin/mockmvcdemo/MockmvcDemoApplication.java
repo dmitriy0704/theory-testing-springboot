fb5474fd4e372f5707c13ff3a590d8f80ce92be4
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-10-24 20:08:16 +0300

    mockmvc-demo: init project

---
+++ mockmvc-demo/src/main/java/dev/folomkin/mockmvcdemo/MockmvcDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.folomkin.mockmvcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockmvcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockmvcDemoApplication.class, args);
+    }
+
+}
